13a9c8e6770d2918f95f82bfdcd465985e0c0f9e
anjali <anjali@logicfirst.io>
2023-12-19 21:44:00 +0530

    utils vagera

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+const connectDB = async()=>{
+    try {
+       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        
+    } catch (error) {
+        console.log('MONGO DB K CONNECTION M ERROR',error)
+        process.exit(1)
+    }
+}
