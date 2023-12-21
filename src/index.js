8ba5cebce3ab8b0b2c79386924eaa09125ad6068
anjali <anjali@logicfirst.io>
2023-12-21 11:45:32 +0530

    server connection

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-
+import mongoose from 'mongoose';
+import express from 'express'; 
+import {connectDB} from "./db/index.js";
 
 dotenv.config({
     path: './.env'
 })
-
+const app = express();
 
 connectDB()
 .then(()=>{
-    application.listen(process.env.PORT || 8000, ()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
     console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
 
     })
